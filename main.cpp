c4b19e140da131bf0976cbadd718f1a4b4f8b0da
Skyzeala <anderson.kirani@gmail.com>
2025-07-15 20:35:58 -0700

    first step towards a triangle

--- main.cpp
+++ main.cpp
@@ -2,35 +2,69 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
+
+
+
 
 #include <iostream>
+#include <stdexcept>
+#include <cstdlib>
 
-int main() {
-    glfwInit();
 
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
-
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-    std::cout << extensionCount << " extensions supported\n";
-
-    glm::mat4 matrix;
-    glm::vec4 vec;
-    auto test = matrix * vec;
-
-    while(!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
+class HelloTriangleVulkan {
+public:
+    void run() { 
+        initWindow(); 
+        initVulkan(); 
+        mainLoop(); 
+        cleanup(); 
     }
 
-    glfwDestroyWindow(window);
+private:
+    GLFWwindow* window;
 
-    glfwTerminate();
+    void initWindow() {
+        glfwInit(); //first thing to do
 
-    return 0;
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); //tells glfw not to initialize opengl context
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); //turn off window resizing, special care? did glew have that?
+        
+        window = glfwCreateWindow(1000, 800, "Vulkan", nullptr, nullptr);
+    
+    }
+
+    void initVulkan() {
+
+    }
+
+    void mainLoop() {
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
+    }
+
+    void cleanup() {
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
+};
+
+
+
+
+
+
+int main() {
+    HelloTriangleVulkan app;
+
+    try {
+        app.run();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+
+    return EXIT_SUCCESS;
+
 }
