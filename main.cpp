bcaf7706c905366330ccea567f36fd95fb4378c7
Skyzeala <anderson.kirani@gmail.com>
2025-07-19 11:10:39 -0700

    validation layers - first step

--- main.cpp
+++ main.cpp
@@ -5,8 +5,47 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <cstring>
+#include <vector>
 
 
+
+const std::vector<const char*> validationLayers = {
+    "VK_LAYER_KHRONOS_validation"
+};
+
+#ifdef NDEBUG
+    const bool enableValidationLayers = false;
+#else
+    const bool enableValidationLayers = true;
+#endif
+
+bool checkValidationLayerSupport() {
+    uint32_t layerCount;
+    vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+    std::vector<VkLayerProperties> availableLayers(layerCount);
+    vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+
+
+    for (const char* layerName : validationLayers) {
+        bool layerFound = false;
+
+        for (const auto& layerProperties : availableLayers) {
+            if (strcmp(layerName, layerProperties.layerName) == 0) {
+                layerFound = true;
+                break;
+            }
+        }
+        
+        if (!layerFound) {
+            return false;
+        }
+    }
+
+    return true;
+}
 
 class HelloTriangleVulkan {
 public:
@@ -36,6 +75,10 @@ private:
     }
 
     void createInstance() {
+        if (enableValidationLayers && !checkValidationLayerSupport()) {
+            throw std::runtime_error("validation layers requested, but not available!");
+        }
+
         VkApplicationInfo appInfo{};
         appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
         appInfo.pApplicationName = "Hello Triangle";
@@ -76,7 +119,6 @@ private:
         for (const auto& extension : extensions) {
             std::cout << '\t' << extension.extensionName << '\n';
         }
-
     }
 
     void mainLoop() {
